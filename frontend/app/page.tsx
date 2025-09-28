dbe4d6a8a6e1e61c9433d02b2b34d98423e0c03d
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2025-09-28 18:11:41 +0200

    Refactor Home component layout and improve styling
    Took 17 minutes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import Image from 'next/image';
 
+import { useEffect, useRef, useState } from 'react';
+
 
 export default function Home() {
 
 
-  return (
+    return (
       <>
           <div className="container mx-auto px-4 py-6 flex items-center justify-between opacity-0 transition-opacity">
                 <h2 className="mr-4"><a href="#start">Jelal Kasso</a></h2>
@@ -22,20 +24,22 @@ export default function Home() {
                 </div>
             </nav>
           </div>
-          <div className="flex min-h-screen flex-col items-center justify-between p-18">
+          <div className="flex min-h-screen flex-col justify-between pl-30 pt-18">
 
             <section id="start">
-                <h1 className="text-[10rem] font-bold"><span>Jelal</span><span className="ml-120">Kasso</span></h1>
+                <h1 className="text-[10rem] font-bold">Jelal Kasso</h1>
                 <div className="">
                     <p className="text-2xl mt-4">Photographer | Videographer | Filmmaker</p>
                 </div>
 
-                <div className="mt-16 flex justify-center">
+                <div className="mt-10 flex">
                     <a
                         href="https://www.instagram.com/jk_fotovideo/" // Hier deine Instagram URL eintragen
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="group relative w-15 h-15 bg-gradient-to-br rounded-full flex items-center justify-center hover:scale-110 transition-all duration-300 shadow-lg hover:shadow-xl border-1"
+                        className="group relative w-15 h-15 bg-gradient-to-br rounded-full flex items-center
+                                   justify-center transition-all duration-300 shadow-lg border-1 border-gray-500
+                                   hover:border-gray-300"
                     >
                         {/* Mit lokalem SVG */}
                         <Image
@@ -43,14 +47,14 @@ export default function Home() {
                             alt="Instagram"
                             width={32}
                             height={32}
-                            className="filter brightness-0 invert group-hover:scale-110 transition-transform duration-300"
+                            className="filter brightness-0 invert transition-transform duration-300"
                         />
 
                     </a>
                 </div>
 
 
-                <div className="mt-150" id="about">
+                <div className="mt-130" id="about">
                     <h2 className="text-4xl font-semibold mb-4">Über mich</h2>
                     <p className="text-lg max-w-3xl">
                         Hi, ich bin JK – leidenschaftlicher Fotograf und Videograf. Ich liebe es, besondere Momente
@@ -64,5 +68,5 @@ export default function Home() {
             </section>
           </div>
       </>
-  );
+    );
 }
