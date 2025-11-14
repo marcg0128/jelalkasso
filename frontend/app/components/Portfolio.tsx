a4e5f3281b2da3e3ed45ae61f0a854440e521713
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2025-11-15 00:04:19 +0100

    Refactor Portfolio and Start components for improved layout and added new portfolio items
    Took 1 hour 37 minutes

--- frontend/app/components/Portfolio.tsx
+++ frontend/app/components/Portfolio.tsx
@@ -22,73 +22,88 @@ export default function Portfolio() {
             title: "Projekt C",
             description: "Beschreibung von Projekt C",
             imageName: "/images/projekt-c.jpg"
+        },
+        {
+            title: "Projekt D",
+            description: "Beschreibung von Projekt A",
+            imageName: "/images/projekt-a.jpg"
+        },
+        {
+            title: "Projekt E",
+            description: "Beschreibung von Projekt B",
+            imageName: "/images/projekt-b.jpg"
+        },
+        {
+            title: "Projekt F",
+            description: "Beschreibung von Projekt C",
+            imageName: "/images/projekt-c.jpg"
         }
     ];
     return (
 
 
             <div className="flex  items-center justify-between gap-4 h-full px-60 ">
-                <div className="flex  items-center justify-center gap-4 h-full">
-                    <div>
-                    <h1
-                        className="text-5xl font-bold mb-6 inline-block after:inline-block
-                        after:ml-20 after:w-20 after:h-0.5 after:bg-[#EAEAEA] after:align-middle
-                        rounded-4xl "
-                    >
-                        Portfolio
-                    </h1>
-                    <div className="mt-10">
-                        <div className="flex gap-10 text-6xl font-extralight ">
-                            <div>
-                               +<CountUp
-                                from={10}
-                                to={40}
-                                separator="."
-                                duration={3}
-                                className="count-up-text"
+                <div className="flex  items-start justify-center gap-4 h-full">
+                    <div className="">
+                        <h1
+                            className="text-5xl font-bold mb-6 inline-block after:inline-block
+                            after:ml-20 after:w-20 after:h-0.5 after:bg-[#EAEAEA] after:align-middle
+                            rounded-4xl "
+                        >
+                            Portfolio
+                        </h1>
+                        <div className="mt-10">
+                            <div className="flex gap-10 text-6xl font-extralight ">
+                                <div>
+                                   +<CountUp
+                                    from={10}
+                                    to={40}
+                                    separator="."
+                                    duration={3}
+                                    className="count-up-text"
 
-                                />
+                                    />
+
+                                </div>
+
+                                <div>
+                                    +<CountUp
+                                    to={8}
+                                    separator="."
+                                    duration={1}
+                                    className="count-up-text"
+
+                                    />
+
+                                </div>
 
                             </div>
-
-                            <div>
-                                +<CountUp
-                                to={8}
-                                separator="."
-                                duration={1}
-                                className="count-up-text"
-
-                                />
+                            <div className="flex text-6xl font-light gap-[130px] text-[#EAEAEA]/80">
+                                <p className="text-center mt-4 text-lg w-7">
+                                    zufriedene Kunden
+                                </p>
+                                <p className="text-center mt-4 text-lg">
+                                    Partner
+                                </p>
 
                             </div>
-
-                        </div>
-                        <div className="flex text-6xl font-light gap-[130px] text-[#EAEAEA]/80">
-                            <p className="text-center mt-4 text-lg w-7">
-                                zufriedene Kunden
-                            </p>
-                            <p className="text-center mt-4 text-lg">
-                                Partner
-                            </p>
 
                         </div>
 
                     </div>
 
-                </div>
-
 
 
                     <div className="flex flex-col items-end ml-20">
-                    {portfolioItems.map((item, index) => (
-                        <div className="mb-10 " key={index}>
+                        {portfolioItems.map((item, index) => (
+                            <div className="mb-10 " key={index}>
 
-                            <h2 className="text-5xl font-bold mt-4">{item.title}</h2>
+                                <h2 className="text-5xl font-bold mt-4">{item.title}</h2>
 
-                        </div>
-                    ))}
+                            </div>
+                        ))}
 
-                </div>
+                    </div>
                 </div>
                 <div>
 
--- frontend/app/components/Start.tsx
+++ frontend/app/components/Start.tsx
@@ -1,13 +1,47 @@
+import Image from "next/image";
+
 export default function Start() {
     // const items =
     return (
-        <div className="min-h-screen md:flex  items-center justify-between  px-60">
-            <div className="flex ">
-                <h1 className="flex flex-col items-start justify-center text-4xl">Jelal Kasso</h1>
-            </div>
-            <div className="bg-amber-50 p-10 rounded-3xl shadow-lg mt-10 md:mt-0">
+        <div className="w-full h-screen px-47">
+            {/*aligns perfect with the title "Jelal Kasso"*/}
+
+            <div className="min-h-screen md:flex flex-col  items-center justify-center ">
+
+                <div className="self-start flex items-start justify-between w-full  ">
+                    <div className="bg-[#1C1C1C] max-w-80 p-5 h-auto flex flex-col items-center justify-center rounded-4xl translate-y-13">
+                        <Image
+                            src="/images/start_foto_sub.jpeg"
+                            alt="Jelal Kasso Logo"
+                            width={300}
+                            height={150}
+                            className="object-contain rounded-2xl"
+                        />
+
+                        <div>
+                            <p className="text-[1.2vw] text-[#EAEAEA]/85  mt-3">Ich erschaffe Bilder & Videos, die Marken bewegen.  </p>
+                        </div>
+                    </div>
+                    <div className="self-end ml-10 max-w-xs translate-y-7">
+                        <p className="text-3xl  text-white">
+                            Meine Mission ist es, mit Fotografie und Film Emotionen zu wecken, Geschichten zu erzählen
+                            und Erinnerungen zu schaffen, die bleiben.
+                        </p>
+                    </div>
+                </div>
+
+                <div className=" self-start px-3 translate-y-15 ">
+                    <p className="text-[1.8vw] text-[#EAEAEA]/85 ">Fotograf</p>
+                    <p className="text-[1.8vw] text-[#EAEAEA]/70 ">Videograf</p>
+                    <p className="text-[1.8vw] text-[#EAEAEA]/55 ">Editer</p>
+                </div>
+                <div className="">
+                    <h1 className="text-[12vw] font-bold ">Jelal&nbsp;&nbsp;&nbsp;&nbsp;Kasso</h1>
+                </div>
 
             </div>
+
         </div>
+
     );
 }
