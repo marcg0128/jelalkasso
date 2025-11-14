28e3f8fb4a555801fce2c732a7872c25e3784df1
Marcos Gomez Alvarez <83846095+marcg0128@users.noreply.github.com>
2025-11-15 00:06:18 +0100

    Add hover effect to title in Start component for enhanced interactivity
    Took 2 minutes

--- frontend/app/components/Start.tsx
+++ frontend/app/components/Start.tsx
@@ -1,12 +1,21 @@
+
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Start() {
-    // const items =
+    const [hover, setHover] = useState(false);
+
+    const titleStyle = (delay: number) => ({
+        opacity: hover ? 1 : 0,
+        transform: hover ? "translateY(0px)" : "translateY(20px)",
+        transition: `all 0.4s ease ${delay}s`
+    });
     return (
         <div className="w-full h-screen px-47">
             {/*aligns perfect with the title "Jelal Kasso"*/}
 
-            <div className="min-h-screen md:flex flex-col  items-center justify-center ">
+            <div className="min-h-screen md:flex flex-col  items-center justify-center mt-10 relative">
 
                 <div className="self-start flex items-start justify-between w-full  ">
                     <div className="bg-[#1C1C1C] max-w-80 p-5 h-auto flex flex-col items-center justify-center rounded-4xl translate-y-13">
@@ -30,13 +39,21 @@ export default function Start() {
                     </div>
                 </div>
 
-                <div className=" self-start px-3 translate-y-15 ">
-                    <p className="text-[1.8vw] text-[#EAEAEA]/85 ">Fotograf</p>
-                    <p className="text-[1.8vw] text-[#EAEAEA]/70 ">Videograf</p>
-                    <p className="text-[1.8vw] text-[#EAEAEA]/55 ">Editer</p>
-                </div>
-                <div className="">
-                    <h1 className="text-[12vw] font-bold ">Jelal&nbsp;&nbsp;&nbsp;&nbsp;Kasso</h1>
+                 <div
+                    style={{ display: "inline-block", cursor: "pointer" }}
+                >
+
+
+                    <div className=" self-start px-3 translate-y-15">
+                        <p className="text-[1.8vw] text-[#EAEAEA]/85" style={titleStyle(0.1)}>Fotograf</p>
+                        <p className="text-[1.8vw] text-[#EAEAEA]/75" style={titleStyle(0.2)}>Videograf</p>
+                        <p className="text-[1.8vw] text-[#EAEAEA]/55" style={titleStyle(0.3)}>Editer</p>
+                    </div>
+                     <h1 className="leading-[0.9] mt-17" style={{ fontSize: "12vw", fontWeight: "bold", marginBottom: "1rem" }}
+                         onMouseEnter={() => setHover(true)}
+                         onMouseLeave={() => setHover(false)}>
+                        Jelal&nbsp;&nbsp;&nbsp;&nbsp;Kasso
+                    </h1>
                 </div>
 
             </div>
